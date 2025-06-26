e3fb5bc94309e8c9e9045e38e13ffe2f46e9d657
gabrielbramley <gabrielbramley@w740.chemy.cf.ac.uk>
2025-06-26 10:13:31 +0100

    Restore atom reordering - FHI-aims should now respect atom ordering

--- embasi/atoms_embedding_asi.py
+++ embasi/atoms_embedding_asi.py
@@ -116,6 +116,7 @@ class AtomsEmbed():
 
     def reorder_atoms_from_embed_mask(self):
         """ Re-orders atoms to push those in embedding region 1 to the beginning
+        of the atom lists to ensure data is organised in a predictable manner.
 
         """
 
