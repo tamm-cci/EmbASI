07f03ef65fedae1d5dd19fd55100d0b8e0ad4f9e
dchen <chendavid771@gmail.com>
2025-07-23 13:44:28 +0100

    Removed unnecessary argument.

--- embasi/workflows/extrapolation.py
+++ embasi/workflows/extrapolation.py
@@ -46,7 +46,7 @@ class Extrapolation:
         Value used for the formula E(∞)
 
     """
-    def __init__(self, file1, file2, path, atom, embed_mask, calc_ll, calc_hl, asi_path, projection1_param = {} , projection2_param = {} , d=2.65, alpha = 4.51, **kwargs):
+    def __init__(self, file1, file2, path, atom, embed_mask, calc_ll, calc_hl, asi_path, projection1_param = {} , projection2_param = {} , d=2.65, alpha = 4.51):
         self.asi_path = asi_path
         os.environ["ASI_LIB_PATH"] = self.asi_path
         self.file1:str= file1
