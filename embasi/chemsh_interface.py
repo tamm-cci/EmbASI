f638e48e83a7d940e7c0fb61c6b43f36e93c5631
eva-ford05 <forde11@cardiff.ac.uk>
2025-07-31 17:03:39 +0100

    Implementation of MultipoleAtoms class.

--- embasi/chemsh_interface.py
+++ embasi/chemsh_interface.py
@@ -5,15 +5,13 @@ try:
 except (ModuleNotFoundError, ValueError):
     ProjectionEmbedding = None
 
-
 class ChemShellInterface(ProjectionEmbedding):
 
-    def __init__(self, atoms, embed_mask, mu_val=1.e+6, ll_calc_config=None, hl_calc_config=None, multipole_atoms=None, **kwargs):
+    def __init__(self, atoms, embed_mask, mu_val=1.e+6, ll_calc_config=None, hl_calc_config=None, multipoles=None, **kwargs):
 
         self.embed_mask = embed_mask
         self.mu_val = mu_val
-        self.multipole_atoms = MultipoleAtoms()
-
+    
         self.ll_calc_config = {'xc':'B3LYP',
         'profile': AimsProfile(command='asi-doesnt-need-command'),
         'KS_method': 'parallel',
@@ -34,50 +32,76 @@ class ChemShellInterface(ProjectionEmbedding):
         'compute_kinetic': True,
         'override_initial_charge_check': True,}
 
-        self.calc_ll = Aims(**self.ll_calc_config)
-        self.calc_hl = Aims(**self.hl_calc_config)
+        self.calc_ll = Aims(**self.ll_calc_config) if ll_calc_config else Aims()
+        self.calc_hl = Aims(**self.hl_calc_config) if hl_calc_config else Aims()
 
+        self._multipoles = multipoles if multipoles else MultipoleAtoms()
+        print(self._multipoles)
+        print(self._multipoles.coords, self._multipoles.charges)
         super().__init__(atoms=atoms, embed_mask=embed_mask,
-            calc_base_ll=self.calc_ll, calc_base_hl=self.calc_hl, mu_val=mu_val)
+            calc_base_ll=self.calc_ll, calc_base_hl=self.calc_hl, mu_val=mu_val, **kwargs)
+
+    @property
+    def multipoles(self):
+        return self._multipoles
 
     def write_multipoles(self):
-        self.multipole_atoms.write_to_geometry_in()
-
-    def add_atoms(self, entry_type, coord, charge, multipole_order=0):
-        self.multipole_atoms.add_atoms(coord, charge, multipole_order)
+        self._multipoles.write_to_geometry_in()
 
 class MultipoleAtoms():
 
     def __init__(self):
-        self._entry_type = []
-        self._coords = []  
-        self._charges = [] 
-        self._multipole_order = 0  
-    
-    def add_atoms(self, entry_type, coord, charge, multipole_order=0):
-        self._entry_type.append()
-        self._coords.append(coord)
-        self._charges.append(charge)
-        self._multipole_order.append(multipole_order)
+        self._multipoles = []
+        
+    def add_multipole(self, coord, charge, multipole_order=0):
+        if not isinstance(coord, (list, tuple)) or len(coord) != 3:
+            raise ValueError("Coordinate must be 3-element list/tuple")
+   
+        self._multipoles.append({'coord': [float(x) for x in coord],
+            'charge': float(charge),'multipole_order': int(order)})
     
     @property
     def coords(self):
-        return self._coords
+        return [e['coord'] for e in self._multipoles]
+
+    @coords.setter
+    def coords(self, val):
+        self._coords=val
     
     @property
     def charges(self):
-        return self._charges
+        return [e['charge'] for e in self._multipoles]
+
+    @charges.setter
+    def charges(self, val):
+        self._charges=val
     
     def write_to_geometry_in(self):
-        import os 
-        cwd = os.getcwd()
-        geometry_path = os.path.join(cwd, "geometry.in")
-        with open(geometry_path, 'a') as f:
-            for i, (entry_type, coord, charge) in enumerate(zip(self._entry_type, self._coords, self._charges)):
-                x, y, z = coord
-                if entry_type == 'atom':
-                    f.write(f"atom {x:16.9f} {y:16.9f} {z:16.9f}\n")
-                    if self._regions[i] is not None:
-                        f.write(f"qm_embedding_region {self._embed_mask[i]}\n")
-                elif entry_type == 'multipole':
-                    f.write(f"multipole {x:16.9f} {y:16.9f} {z:16.9f} {self._multipole_order:2d} {charge:12.8f}\n")
+        if not self._multipoles:
+            print("No multipoles to write")
+            return
+
+        from pathlib import Path
+        base_dir = Path("EmbASI_calc") 
+        calc_dirs = ['AB_LL', 'A_HL', 'A_HL_PP', 'A_LL']
+
+        for calc_dir in calc_dirs:
+            try:
+                geometry_path = base_dir / calc_dir / "geometry.in"
+                geometry_path.parent.mkdir(parents=True, exist_ok=True)
+                if not geometry_path.exists():
+                    print(f"Warning: {geometry_path} doesn't exist - creating new file")
+                    with open(geometry_path, 'w') as f:
+                        f.write("# Created by EmbASI interface\n")
+            
+                print(f"Appending to {geometry_path}")
+                with open(geometry_path, 'a') as f:  
+                    for mp in self._multipoles:
+                        x, y, z = mp['coord']
+                        f.write(
+                        f"multipole {x:16.9f} {y:16.9f} {z:16.9f} "
+                        f"{mp['order']:2d} {mp['charge']:12.8f}\n")
+                        if mp.get('region'):
+                            f.write(f"qm_embedding_region {mp['region']}\n")
+            except Exception as e:
+                print(f"Error writing to {geometry_path}: {str(e)}")
