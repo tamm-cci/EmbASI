d5507b995b46a2f0ca263f22912ec9ef40c379a4
eva-ford05 <forde11@cardiff.ac.uk>
2025-08-12 15:54:12 +0100

    Modification to include charges witin EmbASI calculator

--- embasi/chemsh_interface.py
+++ embasi/chemsh_interface.py
@@ -9,9 +9,11 @@ except (ModuleNotFoundError, ValueError):
 
 class ChemShellInterface(ProjectionEmbedding):
 
-    def __init__(self, embed_mask, mu_val=1.e+6, ll_calc_config=None, hl_calc_config=None, multipoles=None, **kwargs):
+    def __init__(self, embed_mask, mu_val=1.e+6, ll_calc_config=None, hl_calc_config=None, 
+                charge=None, multipoles=None, **kwargs):
         self.embed_mask = embed_mask
         self.mu_val = mu_val
+        self.charge = charge
     
         self.ll_calc_config = {'xc':'B3LYP',
         'profile': AimsProfile(command='asi-doesnt-need-command'),
@@ -21,6 +23,7 @@ class ChemShellInterface(ProjectionEmbedding):
         'density_update_method': 'density_matrix',
         'atomic_solver_xc': "PBE",
         'compute_kinetic': True,
+        'qmmm':True,
         'override_initial_charge_check': True,}
 
         self.hl_calc_config = {'xc':'B3LYP',
@@ -31,6 +34,7 @@ class ChemShellInterface(ProjectionEmbedding):
         'density_update_method': 'density_matrix',
         'atomic_solver_xc': "PBE",
         'compute_kinetic': True,
+        'qmmm': True,
         'override_initial_charge_check': True,}
 
         if ll_calc_config is not None:
@@ -46,7 +50,8 @@ class ChemShellInterface(ProjectionEmbedding):
         self._multipoles = MultipoleAtoms()
 
         super().__init__(embed_mask=self.embed_mask, calc_base_ll=self.calc_ll,
-                        calc_base_hl = self.calc_hl, mu_val=mu_val, **kwargs)
+                        calc_base_hl = self.calc_hl, mu_val=mu_val, 
+                        total_charge=self.charge, **kwargs)
 
     @property
     def multipoles(self):
@@ -59,7 +64,6 @@ class ChemShellInterface(ProjectionEmbedding):
 class MultipoleAtoms():
 
     def __init__(self, symbols=None, positions=None, **kwargs):
-
         self._multipoles = []
         self.coordinates = []
         self.charge = []
